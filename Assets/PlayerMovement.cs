9410297e4e9c6a0084529e846d0bccc8e8f51647
Ninjago77 <shanvantharunmozhi@gmail.com>
2026-05-30 05:00:09 -0700

    good jumping added

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed = 4f;
+    public float sprintSpeed = 14f;
+    [Space]
     public float maxVelocityChange = 10f;
+    //public float XZFriction = .01f;
+    [Space]
+    public float jumpHeight = 1.5f;
 
     private Vector2 input;
     private Rigidbody rb;
+    private bool isSprinting = false;
+    private bool isJumping = false;
 
+    private bool isGrounded = false;
+    private int jumpCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        isGrounded = true;
+        jumpCount = 0;
     }
 
     // Update is called once per frame
@@ -19,11 +35,30 @@ public class PlayerMovement : MonoBehaviour
     {
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         input.Normalize();
+
+        isSprinting = Input.GetButton("Sprint");
+        isJumping = Input.GetButton("Jump");
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+        if (isJumping)
+        {
+            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, rb.linearVelocity.z);
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y + (jumpHeight/(Mathf.Pow(2f,jumpCount))), rb.linearVelocity.z);
+            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, Mathf.Sqrt((2f * Mathf.Abs(Physics.gravity.y) * jumpHeight) + (Mathf.Pow(Mathf.Max(0f, rb.linearVelocity.y), 2f) / 2f)), rb.linearVelocity.z);
+            isGrounded = false;
+            jumpCount++;
+        }
+
+        //if (input.magnitude > 0.5f) {
+            rb.AddForce(CalculateMovement((isSprinting && isGrounded) ? sprintSpeed : walkSpeed), ForceMode.VelocityChange);
+        //} 
+        //else
+        //{
+        //    rb.linearVelocity = new Vector3(rb.linearVelocity.x * XZFriction * Time.fixedDeltaTime, rb.linearVelocity.y, rb.linearVelocity.z * XZFriction * Time.fixedDeltaTime);
+            
+        //}
     }
 
     Vector3 CalculateMovement(float _speed)
@@ -35,7 +70,8 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 currentVelocity = rb.linearVelocity;
 
-        if (input.magnitude > 0.5f) {
+        if (input.magnitude > 0.5f)
+        {
             Vector3 velocityChange = targetVelocity - currentVelocity;
 
             velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
